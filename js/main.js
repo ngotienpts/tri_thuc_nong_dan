a058d3a3bfab52ba311642bde25366ab0cddb3ae
Ngo_van_tien <ngotienpts@gmail.com>
2023-09-04 17:45:14 +0700

    4/9

--- js/main.js
+++ js/main.js
@@ -2,6 +2,16 @@ document.addEventListener("DOMContentLoaded", function () {
     // back top
     var backTop = document.querySelector("#back-top");
 
+    // show sub menu
+    var dropdownSubMenu = document.querySelectorAll(".js__dropDown");
+    var subMenu = document.querySelector(".js__clickShowMenuMb");
+
+    // search mb
+    var searchMbs = document.querySelectorAll(".js__searchMb");
+
+    // show popup image
+    var oneSlides = document.querySelectorAll(".js__swiperItemsContainer");
+
     const app = {
         // su ly cac su kien
         handleEvent: function () {
@@ -14,6 +24,76 @@ document.addEventListener("DOMContentLoaded", function () {
                     document.documentElement.scrollTop = 0;
                 };
             }
+
+            // show sub menu
+            if (subMenu) {
+                var closeSubMenu = document.querySelector(".js__closeSubMenu");
+                var overlay = document.querySelector(".js__overlay");
+                var parentBox = subMenu.parentElement;
+
+                subMenu.onclick = function () {
+                    this.parentElement.classList.add("active");
+                    document.querySelector("body").classList.add("hidden");
+                };
+                closeSubMenu.onclick = function () {
+                    parentBox.classList.remove("active");
+                    document.querySelector("body").classList.remove("hidden");
+                };
+                overlay.onclick = function () {
+                    parentBox.classList.remove("active");
+                    document.querySelector("body").classList.remove("hidden");
+                };
+            }
+
+            // dropdown sub menu
+            dropdownSubMenu &&
+                dropdownSubMenu.forEach((item) => {
+                    var parent = item.parentElement;
+                    var nextEle = parent.querySelector(".js__listSubMenu");
+                    item.onclick = function () {
+                        parent.classList.toggle("active");
+                        if (nextEle.style.maxHeight) {
+                            nextEle.style.maxHeight = null;
+                        } else {
+                            nextEle.style.maxHeight =
+                                nextEle.scrollHeight + "px";
+                        }
+                    };
+                });
+
+            // search mb
+            if (searchMbs) {
+                searchMbs.forEach((searchMb) => {
+                    var closeSearchMb =
+                        document.querySelector(".js__closeSearchMb");
+                    var formSearchMb =
+                        document.querySelector(".js__formSearchMb");
+                    searchMb.onclick = function () {
+                        formSearchMb.classList.add("active");
+                    };
+                    closeSearchMb.onclick = function () {
+                        formSearchMb.classList.remove("active");
+                    };
+                });
+            }
+        },
+        // slider one item
+        sliderOneItems: function () {
+            oneSlides.forEach((item) => {
+                var pagi = item.querySelector(".swiper-pagination");
+                var slider = item.querySelector(".js__swiperItems");
+                new Swiper(slider, {
+                    slidesPerView: 1,
+                    spaceBetween: 30,
+                    slidesPerGroup: 1,
+                    autoHeight: true,
+                    effect: "fade",
+                    pagination: {
+                        el: pagi,
+                        clickable: true,
+                    },
+                });
+            });
         },
         // scroll top
         scrollFunc: function () {
@@ -43,6 +123,8 @@ document.addEventListener("DOMContentLoaded", function () {
         start: function () {
             // su ly cac su kien
             this.handleEvent();
+            // slider one item
+            this.sliderOneItems();
             // window scroll
             this.windowScroll();
         },
